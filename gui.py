f2dd561eff2b52981a5a2ea4c6efe4ce75370d43
Alistair White-Horne <42572235+dapeef@users.noreply.github.com>
2024-07-31 18:50:57 +0100

    Add info and scores
    Also sort formatting error

--- gui.py
+++ gui.py
@@ -2,23 +2,27 @@ import pygame
 import sys
 from game import Game
 import enum
+import time
+
 
 # Initialize Pygame
 pygame.init()
 
 # Variables
 NUM_CARDS_PER_PLAYER = 10
-NUM_PLAYERS = 2
+NUM_PLAYERS = 4
 
 # Constants
 CARD_WIDTH, CARD_HEIGHT = 50, 75
 CARD_CORNER_RADIUS = 5
 CARD_BORDER_THICKNESS = 2
+INFO_FONT_SIZE = 25
 MARGIN = 10
 PLAYER_CARDS_X = MARGIN
-PLAYER_CARDS_Y = 3*CARD_HEIGHT + 7*MARGIN
-WIN_WIDTH = CARD_WIDTH * (NUM_CARDS_PER_PLAYER+1) + MARGIN * (NUM_CARDS_PER_PLAYER+2) + 2*PLAYER_CARDS_X
-WIN_HEIGHT = CARD_HEIGHT * (2*NUM_PLAYERS+1) + MARGIN * (2*NUM_PLAYERS+7)
+PLAYER_CARDS_Y = CARD_HEIGHT * (NUM_PLAYERS+1) + (NUM_PLAYERS+5)*MARGIN
+SCORE_WIDTH = 50
+WIN_WIDTH = CARD_WIDTH * (NUM_CARDS_PER_PLAYER+1) + MARGIN * (NUM_CARDS_PER_PLAYER+5) + SCORE_WIDTH
+WIN_HEIGHT = CARD_HEIGHT * (2*NUM_PLAYERS+1) + MARGIN * (2*NUM_PLAYERS+8) + INFO_FONT_SIZE
 DECK_X, DECK_Y = WIN_WIDTH//2 - CARD_WIDTH - MARGIN//2, 2*MARGIN
 DISCARD_X, DISCARD_Y = WIN_WIDTH//2 + MARGIN//2, 2*MARGIN
 MELD_BUTTON_X, MELD_BUTTON_Y = 2*MARGIN, 2*MARGIN
@@ -35,8 +39,16 @@ TABLE_COLOUR = (161, 102, 47) # Brown
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption('Rummy GUI')
 
-# Font for card text
-font = pygame.font.Font(None, 36)
+# Fonts
+CARD_FONT = pygame.font.Font(None, size=36)
+INFO_FONT = pygame.font.Font(None, size=INFO_FONT_SIZE)
+SCORE_FONT = pygame.font.Font(None, size=30)
+
+# Info variables
+INFO_ON_TIME = 1 # secs
+INFO_FADE_TIME = 1 # secs
+info_text : str = ""
+info_time : float = 0.0
 
 
 # TODO turn this into an enum
@@ -57,7 +69,7 @@ def draw_card(surface:pygame.surface.Surface, x:int, y:int, width:int=CARD_WIDTH
     pygame.draw.rect(surface, border_color, card_rect, CARD_BORDER_THICKNESS, border_radius=CARD_CORNER_RADIUS)
     
     # Draw the text
-    text_surface = font.render(card_text if face_up else "", True, BLACK)
+    text_surface = CARD_FONT.render(card_text if face_up else "", True, BLACK)
     text_rect = text_surface.get_rect(center=card_rect.center)
     surface.blit(text_surface, text_rect)
 
@@ -118,7 +130,28 @@ def draw_melds(surface:pygame.surface.Surface, game:Game) -> None:
         
         x_pos += MARGIN
 
-def check_card_click(position:tuple, card_rects:dict[str, pygame.Rect], game:Game, state:GUIState) -> None:
+def draw_scores(surface:pygame.surface.Surface, game:Game) -> None:
+    for i, score in enumerate(game.scores):
+        text_surface = SCORE_FONT.render(str(score), True, BLACK)
+        text_rect = text_surface.get_rect(center=(WIN_WIDTH - MARGIN - SCORE_WIDTH//2, PLAYER_CARDS_Y + MARGIN + i * (CARD_HEIGHT + MARGIN) + CARD_HEIGHT//2))
+        surface.blit(text_surface, text_rect)
+
+def draw_info(surface:pygame.surface.Surface) -> None:
+    colour_multiplier = pygame.math.clamp((time.time() - info_time - INFO_ON_TIME)/INFO_FADE_TIME, 0, 1)
+    text_surface = INFO_FONT.render(info_text, True, [255 * colour_multiplier]*3)
+    text_rect = text_surface.get_rect(center=(WIN_WIDTH//2, WIN_HEIGHT - MARGIN - INFO_FONT_SIZE//2))
+    surface.blit(text_surface, text_rect)
+
+
+def show_info(text:str):
+    global info_text
+    global info_time
+
+    info_text = str(text)
+    info_time = time.time()
+
+
+def check_button_click(position:tuple, card_rects:dict[str, pygame.Rect], game:Game, state:GUIState) -> None:
     """Check if a card is clicked and call a function."""
     for id, rect in card_rects.items():
         if rect.collidepoint(position):
@@ -145,12 +178,14 @@ def check_card_click(position:tuple, card_rects:dict[str, pygame.Rect], game:Gam
                         game.draw(player=game.whose_go, from_deck=True)
                     except AssertionError as e:
                         print(e)
+                        show_info(e)
                 elif id == "discard":
                     print("DISCARD")
                     try:
                         game.draw(player=game.whose_go, from_deck=False)
                     except AssertionError as e:
                         print(e)
+                        show_info(e)
                 elif id[:4] == "card":
                     # Parse player and card index
                     _, player, card_index = [i for i in id.split("-")]
@@ -164,6 +199,7 @@ def check_card_click(position:tuple, card_rects:dict[str, pygame.Rect], game:Gam
                         game.discard(player=player, card_index=card_index)
                     except AssertionError as e:
                         print(e)
+                        show_info(e)
             
             else:
                 # Meld selection mode
@@ -182,12 +218,14 @@ def check_card_click(position:tuple, card_rects:dict[str, pygame.Rect], game:Gam
                             state.meld_selected.pop(state.meld_selected.index(card_index))
                     else:
                         print(f"Can't touch that card, it's player {game.whose_go}'s turn, not player {player}")
+                        show_info(f"Can't touch that card, it's player {game.whose_go}'s turn, not player {player}")
 
             break
 
+
 def main() -> None:
     # Initialise game
-    game = Game()
+    game = Game(NUM_PLAYERS)
 
     # game.melds = [["A", "B", "C"], ["A", "B", "C", "D"], ["A", "B", "C"], ["A", "B", "C"], ["A", "B", "C"], ["A", "B", "C"]]
 
@@ -208,7 +246,7 @@ def main() -> None:
             
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Left mouse button
-                    check_card_click(event.pos, hit_boxes, game, state)
+                    check_button_click(event.pos, hit_boxes, game, state)
         
         screen.fill(WHITE)
 
@@ -216,7 +254,7 @@ def main() -> None:
         pygame.draw.rect(
             screen,
             TABLE_COLOUR,
-            pygame.Rect(MARGIN, MARGIN, WIN_WIDTH - 2*MARGIN, CARD_HEIGHT * (NUM_PLAYERS+1) + 5*MARGIN),
+            pygame.Rect(MARGIN, MARGIN, WIN_WIDTH - 2*MARGIN, CARD_HEIGHT * (NUM_PLAYERS+1) + (NUM_PLAYERS+3)*MARGIN),
             border_radius=CARD_CORNER_RADIUS + MARGIN
         )
 
@@ -227,12 +265,18 @@ def main() -> None:
         pygame.draw.rect(
             screen,
             GREEN,
-            pygame.Rect(PLAYER_CARDS_X, PLAYER_CARDS_Y + (CARD_HEIGHT+MARGIN)*game.whose_go, WIN_WIDTH - 2*PLAYER_CARDS_X, CARD_HEIGHT + 2*MARGIN),
+            pygame.Rect(PLAYER_CARDS_X, PLAYER_CARDS_Y + (CARD_HEIGHT+MARGIN)*game.whose_go, (CARD_WIDTH+MARGIN) * (NUM_CARDS_PER_PLAYER+1) + MARGIN, CARD_HEIGHT + 2*MARGIN),
             border_radius=CARD_CORNER_RADIUS + MARGIN
         )
 
         # Draw clickable items and save hitboxes
         hit_boxes : dict[str, pygame.Rect] = draw_all_clickable(screen, game, state)
+
+        # Draw scores
+        draw_scores(screen, game)
+
+        # Draw info
+        draw_info(screen)
         
         pygame.display.flip()
         clock.tick(60)
